c65b09205c25082ca3967a89c446dab46732be4c
NileshVirkar <nilesh.virkar@acellere.com>
2017-07-11 11:40:54 +0200

    added tests

--- subproject2/src/main/java/subproject2/DemoSubproject2.java
+++ subproject2/src/main/java/subproject2/DemoSubproject2.java
@@ -2,4 +2,16 @@ package subproject2;
 
 public class DemoSubproject2 {
 
+	public static void main(String[] args) {
+		int k_subproject;
+	}
+	
+	public boolean unusedMethodSubproject(boolean k) {
+		k = true;
+		
+        String foo = null;
+        System.out.println(foo.length());
+        
+        return k;
+	}
 }
