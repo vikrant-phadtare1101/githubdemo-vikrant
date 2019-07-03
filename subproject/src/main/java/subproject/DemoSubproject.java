7af6def0989a2ec96d54205745f90e5c216ffcfd
NileshVirkar <nilesh.virkar@acellere.com>
2019-07-03 14:08:29 +0530

    commit2

--- subproject/src/main/java/subproject/DemoSubproject.java
+++ subproject/src/main/java/subproject/DemoSubproject.java
@@ -8,26 +8,10 @@ public class DemoSubproject {
 	}
 	
 	private static DemoSubproject foo = null;
-
-	public boolean unusedMethodSubproject(boolean k) {
-		k = true;
-		
-		int subproject;
-
-
-try {
-        String foo = null;
-        System.out.println(foo.length());
-}catch(Exception e) {
 	
-}
 	
+	//multiple simultaneous callers may see partially initialized objects
 
-
-        return k;
-	}
-	
-	multiple simultaneous callers may see partially initialized objects
  public static DemoSubproject getFoo() {
      if (foo==null) {
          foo = new DemoSubproject();
