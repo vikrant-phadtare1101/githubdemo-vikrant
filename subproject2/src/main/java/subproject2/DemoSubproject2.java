edcd3a7ea9fc55b253987a81bc600bc777f51bb2
NileshVirkar <nilesh.virkar@acellere.com>
2019-07-04 14:48:50 +0530

    commit_message [gamma_force_commit] Gamma Quality Gate:Failed, Total added NonThreadSafeSingleton issues (1) are more than quality gate error threshold(0)

--- subproject2/src/main/java/subproject2/DemoSubproject2.java
+++ subproject2/src/main/java/subproject2/DemoSubproject2.java
@@ -8,15 +8,6 @@ public class DemoSubproject2 {
 		int k_subproject;
 	}
 
-	public boolean unusedMethodSubproject(boolean k) {
-		k = true;
-
-		String foo = null;
-		System.out.println(foo.length());
-
-		return k;
-	}
-
 
 	private static DemoSubproject2 foo = null;
 
@@ -26,4 +17,14 @@ public class DemoSubproject2 {
 		}
 		return foo;
 	}
+	
+	public boolean unusedMethodSubproject(boolean k) {
+		k = true;
+
+		String foo = null;
+		System.out.println(foo.length());
+
+		return k;
+	}
+
 }
