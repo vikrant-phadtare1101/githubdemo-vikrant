6d2ea414e48bd5427dad337434c1cbebdde830a3
NileshVirkar <nilesh.virkar@acellere.com>
2019-07-23 16:33:51 +0530

    commit message

--- subproject/src/main/java/subproject/DemoSubproject.java
+++ subproject/src/main/java/subproject/DemoSubproject.java
@@ -33,6 +33,7 @@ public class DemoSubproject {
 	    System.out.println("");
 	    System.out.println("");
 	    System.out.println("");
+	    System.out.println("");
 	    
      if (foo==null) {
          foo = new DemoSubproject();
