434e7e246cf500ffa67466b71eac928850af64d2
NileshVirkar <nilesh.virkar@acellere.com>
2019-07-28 22:34:25 +0530

    commit message [gamma_force_commit] [Gamma Quality Gate:Failed] subproject/src/main/java/subproject/DemoSubproject.java NonThreadSafeSingleton CRITICAL 84 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 39 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 40 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 41 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 42 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 43 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 44 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 45 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 46 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 47 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 48 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 49 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 50 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 51 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 52 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 53 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 54 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 55 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 56 subproject/src/main/java/subproject/DemoSubproject.java UseOfSysout HIGH 57
    and more...

--- subproject/src/main/java/subproject/DemoSubproject.java
+++ subproject/src/main/java/subproject/DemoSubproject.java
@@ -36,6 +36,51 @@ public class DemoSubproject {
 	    System.out.println("");
 	    System.out.println("");  
 	          
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println(""); 
+	    
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println(""); 
      if (foo==null) {
          foo = new DemoSubproject();
      }
