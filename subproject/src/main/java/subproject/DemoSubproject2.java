1a997a2341ab94c629be4406f1601627d0251c57
NileshVirkar <nilesh.virkar@acellere.com>
2019-08-02 13:22:06 +0530

    branch1 commit

--- subproject/src/main/java/subproject/DemoSubproject2.java
+++ subproject/src/main/java/subproject/DemoSubproject2.java
@@ -14,6 +14,7 @@ public class DemoSubproject2 {
 
  public static DemoSubproject getFoo() {
 	 System.out.println("commit 3");
+	 System.out.println("commit 3");
      if (foo==null) {
          foo = new DemoSubproject();
      }
@@ -21,6 +22,7 @@ public class DemoSubproject2 {
  }
 
  public static DemoSubproject getFoot() {
+	 System.out.println("commit 3");
      if (foo==null) {
          foo = new DemoSubproject();
      }
