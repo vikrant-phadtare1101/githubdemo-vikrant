00434af20fae74db6a109b46c585296e1a308c53
NileshVirkar <nilesh.virkar@acellere.com>
2020-03-27 22:00:28 +0530

    commit

---
+++ subproject2/subproject2_A/subproject2_A_1/src/main/java/org/subproject2_A_1/Demo.java
@@ -0,0 +1,25 @@
+package org.subproject2_A_1;
+
+public class Demo {
+
+    public static void main( String[] args )
+    {
+    	try {	
+    	} catch (Exception e) {
+		}
+        System.out.println( "Hello World!" );
+        System.out.println("");
+    }
+}
+
+class Demo2 {
+
+    public static void main( String[] args )
+    {
+    	try {	
+    	} catch (Exception e) {
+		}
+        System.out.println( "Hello World!" );
+        System.out.println("");
+    }
+}
