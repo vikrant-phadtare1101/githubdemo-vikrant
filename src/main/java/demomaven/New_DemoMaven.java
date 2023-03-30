17ac4fdd74ab1e359a21e16bf62f015647e8dfb3
Nilesh Virkar <nilesh.virkar@acellere.com>
2023-03-30 18:19:48 +0530

    Add files via upload

---
+++ src/main/java/demomaven/New_DemoMaven.java
@@ -0,0 +1,18 @@
+package demomaven;
+
+public class New_DemoMaven {
+
+	public static void main(String[] args) {
+		//int k;
+	}
+	
+	public boolean unusedMethod(boolean k) {
+		k = true;
+		return k;
+	}
+	
+	public boolean unusedMethod2() {
+	    int z;
+	}
+
+}
