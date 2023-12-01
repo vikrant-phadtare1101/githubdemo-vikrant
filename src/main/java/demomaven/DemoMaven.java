b720e752cfc3d09f8dff4efd3ddff90924fb2b60
Nilesh <nilesh.virkar@acellere.com>
2023-12-01 18:17:27 +0530

    Revert "Delete src/main/java/demomaven/DemoMaven.java"
    This reverts commit 657fa1d8efdce08715762eaf1870b900a362dde5.

---
+++ src/main/java/demomaven/DemoMaven.java
@@ -0,0 +1,27 @@
+package demomaven;
+
+public class DemoMaven {
+
+	public static void main(String[] args) {
+		int k;
+	}
+	
+	public boolean unusedMethod(boolean k) { 
+		k = true;
+		int t;
+		
+		
+		
+		
+		
+		
+        
+		return k;
+	}
+	
+	public boolean unusedMethod2(boolean k) {
+		boolean p=true;
+		return p;
+	}
+
+}
