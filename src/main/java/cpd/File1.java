6160daf87c19e7377856837ce2745f64c0709cd6
Nilesh Virkar <nilesh.virkar@acellere.com>
2024-06-04 22:53:10 +0530

    Create File1.java

---
+++ src/main/java/cpd/File1.java
@@ -0,0 +1,40 @@
+package demomaven;
+
+public class File1 {
+
+	public static void main(String[] args) {
+		int k;
+	}
+	
+	public boolean unusedMethod(boolean k) {
+		
+		k = true;
+		
+        String foo = null;
+
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+    System.out.println(foo.length());
+        
+		return k;
+	}
+	
+	public boolean unusedMethod2(boolean k) {
+		k = true;
+		
+        String foo = null;
+		
+		
+		
+		
+        System.out.println(foo.length());
+        
+		return k;
+	}
+
+}
